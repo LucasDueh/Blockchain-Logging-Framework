6e5412e612c00ad1ede6b4c9294ba9bfa5ab21a6
Lucas Dührsen <lucas.duehrsen@tum.de>
2022-04-19 19:50:46 +0200

    Add method isValidTransactionInput which uses filter predicate

--- src/main/java/blf/blockchains/ethereum/instructions/EthereumTransactionInputDecodingFilterInstruction.java
+++ src/main/java/blf/blockchains/ethereum/instructions/EthereumTransactionInputDecodingFilterInstruction.java
@@ -5,6 +5,7 @@ import blf.blockchains.ethereum.reader.EthereumDataReader;
 import blf.blockchains.ethereum.state.EthereumProgramState;
 import blf.core.state.ProgramState;
 import blf.core.instructions.Instruction;
+import blf.core.interfaces.FilterPredicate;
 import io.reactivex.annotations.NonNull;
 
 import java.util.List;
@@ -13,13 +14,16 @@ import java.util.List;
  * Transaction Input Decoding Scope
  */
 public class EthereumTransactionInputDecodingFilterInstruction extends Instruction {
+    private final FilterPredicate<String> transactionInputCriterion;
     private final EthereumTransactionInputDecoding decoding;
 
     public EthereumTransactionInputDecodingFilterInstruction(
+        @NonNull FilterPredicate<String> transactionInputCriterion,
         @NonNull EthereumTransactionInputDecoding decoding,
         List<Instruction> instructions
     ) {
         super(instructions);
+        this.transactionInputCriterion = transactionInputCriterion;
         this.decoding = decoding;
     }
 
@@ -29,9 +33,16 @@ public class EthereumTransactionInputDecodingFilterInstruction extends Instructi
         final EthereumDataReader ethereumReader = ethereumProgramState.getReader();
 
         String input = ethereumReader.getCurrentTransaction().getInput();
+        String functionIdentifier = input.substring(2, 10);
 
-        decoding.decode(input, ethereumProgramState);
+        if (this.isValidTransactionInput(state, functionIdentifier)) {
+            decoding.decode(input, ethereumProgramState);
 
-        this.executeNestedInstructions(state);
+            this.executeNestedInstructions(state);
+        }
+    }
+
+    private boolean isValidTransactionInput(ProgramState state, String functionIdentifier) {
+        return this.transactionInputCriterion.test(state, functionIdentifier);
     }
 }
